48cec64f62e8e4dbac59d0b3cf33913a1f792aad
jayparekh4801 <parekhjay4801@gmail.com>
2021-05-17 18:29:47 +0530

    scraped data can be added into mysql and mongo database

--- GithubSeeker/spiders/githubseeker.py
+++ GithubSeeker/spiders/githubseeker.py
@@ -7,6 +7,7 @@ import os
 import csv
 import pandas
 from openpyxl import Workbook
+import pymongo
 
 
 class GithubseekerSpider(scrapy.Spider):
@@ -42,27 +43,38 @@ class GithubseekerSpider(scrapy.Spider):
         
     
     def close(self, reason) :
-        csv_file = max(glob.iglob('*.csv'), key=os.path.getctime)
+        # csv_file = max(glob.iglob('*.csv'), key=os.path.getctime)
 
         #  code to put data in mysql database 
-        mydb = mysql.connector.connect(
-            host = "127.0.0.1",
-            user = "root",
-            password = "vnhmhJi#7ms",
-            database = "scrapy"
-        )
-        mycursor = mydb.cursor()
-        createTable = "CREATE TABLE GITDATA(name VARCHAR(200), userName VARCHAR(200), current_work VARCHAR(200), work_loc VARCHAR(200), no_repos VARCHAR(200), tot_commits VARCHAR(200))"
-        mycursor.execute(createTable)
-        csv_data = csv.reader(open(csv_file))
-        row_count = 0
-        for row in csv_data:
-            if row_count != 0 :
-                insertInto = ("INSERT IGNORE INTO GITDATAo(name, userName, image_urls, biodata, current_work, work_loc, no_repos, lis_popular_repos, tot_commits) VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
-                mycursor.execute(insertInto, row)
-            row_count += 1
+        # mydb = mysql.connector.connect(
+        #     host = "127.0.0.1",
+        #     user = "root",
+        #     password = "vnhmhJi#7ms",
+        #     database = "scrapy"
+        # )
+        # mycursor = mydb.cursor()
+        # createTable = "CREATE TABLE GITDATA(name VARCHAR(200), userName VARCHAR(200), current_work VARCHAR(200), work_loc VARCHAR(200), no_repos VARCHAR(200), tot_commits VARCHAR(200))"
+        # mycursor.execute(createTable)
+        # csv_data = csv.reader(open(csv_file))
+        # row_count = 0
+        # for row in csv_data:
+        #     if row_count != 0 :
+        #         insertInto = ("INSERT IGNORE INTO GITDATAo(name, userName, image_urls, biodata, current_work, work_loc, no_repos, lis_popular_repos, tot_commits) VALUE (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+        #         mycursor.execute(insertInto, row)
+        #     row_count += 1
 
-        mydb.commit()
-        mycursor.close()
+        # mydb.commit()
+        # mycursor.close()
 
         #  code to put data in mongodb database
+
+        jl_file = max(glob.iglob('*.jl'), key = os.path.getctime)
+        mongo_connector = pymongo.MongoClient("mongodb://localhost/")
+        database = mongo_connector['gitDatabase']
+        collection = database['gitUserData']
+
+        with open(jl_file) as fp :
+            for row in fp.readlines() :
+                gitRow = {"data" : row}
+                collection.insert_one(gitRow)
+            
